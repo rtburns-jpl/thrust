2e71c5e3eaaa7da3bb6a34642d4eab1043b227a2
Ryan Burns <rtburns-jpl@users.noreply.github.com>
2020-04-27 20:19:03 -0700

    Simplify minimum_type

--- thrust/detail/type_traits/minimum_type.h
+++ thrust/detail/type_traits/minimum_type.h
@@ -78,23 +78,14 @@ struct any_conversion
 
 } // end minimum_type_detail
 
-template<typename T1,
-         typename T2  = minimum_type_detail::any_conversion,
-         typename T3  = minimum_type_detail::any_conversion,
-         typename T4  = minimum_type_detail::any_conversion,
-         typename T5  = minimum_type_detail::any_conversion,
-         typename T6  = minimum_type_detail::any_conversion,
-         typename T7  = minimum_type_detail::any_conversion,
-         typename T8  = minimum_type_detail::any_conversion,
-         typename T9  = minimum_type_detail::any_conversion,
-         typename T10 = minimum_type_detail::any_conversion,
-         typename T11 = minimum_type_detail::any_conversion,
-         typename T12 = minimum_type_detail::any_conversion,
-         typename T13 = minimum_type_detail::any_conversion,
-         typename T14 = minimum_type_detail::any_conversion,
-         typename T15 = minimum_type_detail::any_conversion,
-         typename T16 = minimum_type_detail::any_conversion>
+template<typename T, typename... Ts>
   struct minimum_type;
+
+template<typename T>
+  struct minimum_type<T>
+{
+  typedef T type;
+};
 
 // base case
 template<typename T1, typename T2>
@@ -111,47 +102,14 @@ template<typename T1, typename T2>
 {};
 
 // carefully avoid referring to a nested ::type which may not exist
-template<typename T1,  typename T2,  typename T3,  typename T4,
-         typename T5,  typename T6,  typename T7,  typename T8,
-         typename T9,  typename T10, typename T11, typename T12,
-         typename T13, typename T14, typename T15, typename T16>
-  struct minimum_type
+template<typename T1, typename T2, typename T3, typename... Ts>
+  struct minimum_type<T1, T2, T3, Ts...>
     : lazy_minimum_type<
-        lazy_minimum_type<
-          lazy_minimum_type<
-            minimum_type<
-              T1,T2
-            >,
-            minimum_type<
-              T3,T4
-            >
-          >,
-          lazy_minimum_type<
-            minimum_type<
-              T5,T6
-            >,
-            minimum_type<
-              T7,T8
-            >
-          >
+        minimum_type<
+          T1, T2
         >,
-        lazy_minimum_type<
-          lazy_minimum_type<
-            minimum_type<
-              T9,T10
-            >,
-            minimum_type<
-              T11,T12
-            >
-          >,
-          lazy_minimum_type<
-            minimum_type<
-              T13,T14
-            >,
-            minimum_type<
-              T15,T16
-            >
-          >
+        minimum_type<
+          T3, Ts...
         >
       >
 {};
